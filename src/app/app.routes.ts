4abab509cd7bd8555f6175cee96b9e1497f668af
Pamela Erraes <pamela_erraez@hotmail.com>
2025-03-15 17:42:36 -0500

    He corregido las rutas.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,14 @@
+import { UpdateuserComponent } from './pages/updateuser/updateuser.component';
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { UserComponent } from './pages/user/user.component';
+import { NewuserComponent } from './pages/newuser/newuser.component';
 
 export const routes: Routes = [
-  { path: "", pathMatch:'full',  component: HomeComponent },
+  { path: "", pathMatch:'full',  redirectTo:"home" },
+  { path: "home", component:HomeComponent },
   { path: "user/:id", component: UserComponent },
-  { path: "newuser", component: UserComponent },
-  { path: "updateuser/:id", component: UserComponent },
+  { path: "newuser", component: NewuserComponent },
+  { path: "updateuser/:id", component: UpdateuserComponent },
   { path: "**", redirectTo: 'home' },
 ];
