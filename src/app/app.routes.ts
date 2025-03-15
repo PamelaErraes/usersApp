6d1822651d0768f110d24e5e891d1936546e92f0
Pamela Erraes <pamela_erraez@hotmail.com>
2025-03-15 14:53:40 -0500

    Creación de Componentes y Rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { UserComponent } from './pages/user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", pathMatch:'full',  component: HomeComponent },
+  { path: "user/:id", component: UserComponent },
+  { path: "newuser", component: UserComponent },
+  { path: "updateuser/:id", component: UserComponent },
+  { path: "**", redirectTo: 'home' },
+];
